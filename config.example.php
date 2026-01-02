99c00b9a59d5773e3d4df08009ae2d274e3b8804
CurtisHumphreys <0curtis0@gmail.com>
2026-01-02 20:37:46 +1030

    Add config example file

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Example configuration file.
+ * Copy this to config.php and fill in real values.
+ */
+
+$db_host = "localhost";
+$db_user = "DB_USERNAME";
+$db_pass = "DB_PASSWORD";
+$db_name = "DB_NAME";
+
+$base_url = "https://example.com";
